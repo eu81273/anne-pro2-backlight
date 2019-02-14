25f2a453125c6ca7e34e99c4f5f931f5ee7d72e5
aiden.ahn <aiden.ahn@kakaocorp.com>
2019-02-14 13:39:26 +0900

    Fix bug :wrench:

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ async function getDevices () {
 
                 device.readTimeout(100);
                 device.on('data', _ => device.close() | process.kill(process.pid));
+                // device.on('data', d => console.log(d.map(i => i.toString()).join(' ')));
                 device.on('error', _ => null);
 
                 return device;
@@ -29,7 +30,12 @@ async function getDevices () {
 
     for (let device of devices) {
         if (device) {
-            device.write([ 123, 16, 49, 16, 3, 1, 2, 125, 32, 1, 0 ]);
+            // [123, 16, o, 16, a.length, 1, 2, 125];
+            // o : (e, t) => (t << 4) + e
+            // a : additional data
+            device.write([ 123, 16, 49, 16, 3, 1, 2, 125, 32, 5, 0 ]); // F9
+            // device.write([ 123, 16, 49, 16, 3, 1, 2, 125, 32, 1, 0 ]); // F10
+            setTimeout(device.close.bind(device), 500);
         }
     }
 } ()
